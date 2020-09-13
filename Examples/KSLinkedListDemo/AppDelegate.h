bca8b0f09b2ab8622eaecaaceacafe8a0fda8d08
klaudz <klaudzliang@gmail.com>
2020-09-13 22:40:32 +0800

    feature(Examples): Add the demo project

---
+++ Examples/KSLinkedListDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KSLinkedListDemo
+//
+//  Created by klaudz on 2020/9/13.
+//  Copyright © 2020 Klaudz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
